5434cedf943473e0d69ae4330840a5104ab8e1e6
Emerson <lopes.ds@live.com>
2024-05-12 08:27:40 -0300

    chore() primeiro commit

---
+++ src/main/java/br/com/fiapmsclientess/FiapMsclientessApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiapmsclientess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapMsclientessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiapMsclientessApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiapmsclientess/FiapMsclientessApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiapmsclientess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapMsclientessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
