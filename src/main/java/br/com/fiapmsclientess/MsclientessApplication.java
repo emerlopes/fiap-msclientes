e9e607efdb363188a4178f565bd4929a549a37cc
Emerson <lopes.ds@live.com>
2024-05-12 08:53:22 -0300

    chore() atualizando nome da classe

--- src/main/java/br/com/fiapmsclientess/MsclientessApplication.java
+++ src/main/java/br/com/fiapmsclientess/MsclientessApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FiapMsclientessApplication {
+public class MsclientessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FiapMsclientessApplication.class, args);
+        SpringApplication.run(MsclientessApplication.class, args);
     }
 
 }
