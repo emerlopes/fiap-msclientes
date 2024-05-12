2c8b708961f17207c86b498b991accee3cb0dc50
Emerson <lopes.ds@live.com>
2024-05-12 09:04:10 -0300

    chore() criando classe de atributo cliente

---
+++ src/main/java/br/com/fiapmsclientess/application/dto/Cliente.java
@@ -0,0 +1,31 @@
+package br.com.fiapmsclientess.application.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Accessors(chain = true)
+public class Cliente {
+
+    @JsonProperty("nome")
+    @NotBlank(message = "O nome do cliente e obrigatorio")
+    private String nome;
+
+    @JsonProperty("endereco")
+    @NotBlank(message = "O endereco do cliente e obrigatorio")
+    private String endereco;
+
+    @JsonProperty("telefone")
+    @NotBlank(message = "O telefone do cliente e obrigatorio")
+    @Pattern(regexp = "^\\(?(\\d{2})\\)?[ .-]?(\\d{4,5})[ .-]?(\\d{4})$", message = "Formato de telefone invalido")
+    private String telefone;
+
+    @JsonProperty("email")
+    @NotBlank(message = "O email do cliente e obrigatorio")
+    @Email(message = "O email do cliente e invalido")
+    private String email;
+}
